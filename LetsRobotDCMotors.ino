daa453176d5d24f9cc7ee84764068437a161db66
John J Hritz <john-j-hritz@sbcglobal.net>
2019-02-04 11:05:36 -0800

    Removed Serial feedback to stop buffer flooding

--- LetsRobotDCMotors.ino
+++ LetsRobotDCMotors.ino
@@ -1,8 +1,8 @@
 #include "simple_hbridge.h"
 
-int fspeed= 200;                        /// The "full" speed the motors are allowed to run at normally.
-int hspeed = 125;                       /// The "half" speed the motors will run at.
-int bspeed = fspeed + 55;               /// The "boost" speed the motors will run at under special circumstances.
+int fspeed= 220;                        /// The "full" speed the motors are allowed to run at normally.
+int hspeed = 75;                       /// The "half" speed the motors will run at.
+int bspeed = fspeed + 15;               /// The "boost" speed the motors will run at under special circumstances.
 int cspeed = fspeed;                    /// The current speed the motors are set to run at.
 int bufferSize = 0;
 long finishCommandTimestamp = 0;        /// The program time at which the motors will stop on their own.
@@ -28,7 +28,7 @@ void loop()
 
     if (currentTimestamp >= finishBoostTimestamp)
     {
-        cspeed = hspeed;
+        cspeed = fspeed;
     }
 
     if (Serial.available())
@@ -36,8 +36,8 @@ void loop()
         char inByte = Serial.read();
         delay(2);
 
-        if (Serial.available()) Serial.println("still stuff in the serial stream");
-        else Serial.println("stream empty");
+        //if (Serial.available()) Serial.println("still stuff in the serial stream");
+        //else Serial.println("stream empty");
 
         while (Serial.available()) {
             char tempByte = Serial.read();
@@ -45,43 +45,43 @@ void loop()
             if (tempByte != 10 && tempByte != 13) inByte = tempByte;  // Keep reading bytes until you have the last one.
         }
 
-        Serial.println("Last data read: "+(String)inByte);
+        //Serial.println("Last data read: "+(String)inByte);
 
         switch (inByte)
         {
             //______________Motors______________
             case 'f': // forward
-                Serial.println("Forward!");
+                //Serial.println("Forward!");
                 left_motor(cspeed);
                 right_motor(cspeed);
                 reset_ending_timestamp();
                 break;
             case 'b': // back
-                Serial.println("Back!");
+                //Serial.println("Back!");
                 left_motor(-cspeed);
                 right_motor(-cspeed);
                 reset_ending_timestamp();
                 break;
             case 'r': // right
-                Serial.println("Right!");
+                //Serial.println("Right!");
                 left_motor(cspeed);
                 right_motor(-hspeed);
                 reset_ending_timestamp();
                 break;
             case 'l': // left
-                Serial.println("Left!");
+                //Serial.println("Left!");
                 left_motor(-hspeed);
                 right_motor(cspeed);
                 reset_ending_timestamp();
                 break;
             case 's': // stop
-                Serial.println("Stop!!"); // For debugging only. The pi does not send this.
+                //Serial.println("Stop!!"); // For debugging only. The pi does not send this.
                 left_motor(0);
                 right_motor(0);
                 reset_ending_timestamp();
                 break;
             case 't': // BOOST!
-                Serial.println("BOOST!!!");
+                //Serial.println("BOOST!!!");
                 cspeed = bspeed;
                 set_boost_timestamp(60);
                 break;
